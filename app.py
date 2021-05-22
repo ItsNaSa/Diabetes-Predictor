6772ac78cd0af15034cdc9cc2be87443fd394894
Sahil Naphade <38909890+ItsNaSa@users.noreply.github.com>
2021-05-22 19:25:11 +0530

    Frontend created, need to make changes

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template, request, flash, redirect
+import re
+from flask import Flask, Blueprint, render_template, request, flash, redirect
 from flask.helpers import url_for
-from flask_wtf import FlaskForm
-from wtforms import Form, validators, IntegerField, DecimalField, SubmitField
+from flask_bootstrap import Bootstrap
+from forms import diabetesdataform
+import pickle
+
 import os
 
 SECRET_KEY = os.urandom(32)
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 app.config.update(dict(
     SECRET_KEY="D4E5D124A6074C097A9A4DD39A6197AFD2F2736B",
     WTF_CSRF_SECRET_KEY="3062657BB8605C5BF791641664B38721F27CAC21"
 ))
-
-class diabetesdataform(FlaskForm):
-    pregnancies = IntegerField("Number of pregnancies", [validators.Required("Please enter your number of pregnancies")])
-    glucose = IntegerField("Your glucose level", [validators.Required("Please enter your glucose level")])
-    bp = IntegerField("Your blood pressure", [validators.Required("Please enter your Blood pressure")])
-    st = IntegerField("Your Skin Thickness", [validators.Required("Enter the skin thickness")])
-    insulin = IntegerField("Insulin", [validators.Required("Enter insulin level")])
-    bmi = DecimalField("BMI", [validators.Required("Enter your BMI")])
-    age = IntegerField("Age", [validators.Required("Enter your age")])
-    submit = SubmitField("Predict")
 
 @app.route('/')
 def index():
@@ -31,8 +27,10 @@ def home():
     return render_template('index.html')
 
 # Page to accept the values
-@app.route('/predictor', methods=["POST"])
+@app.route('/predictor', methods=["GET", "POST"])
 def predictor():
-    form = diabetesdataform()
+    form = diabetesdataform(request.form)
+    if request.method == "POST" and form.validate():
+        print("hello")
     # Logic to predict and redirect to the predicted value display
     return render_template('predictorPage.html', form=form)
---
+++ forms.py
@@ -0,0 +1,12 @@
+from wtforms import validators, IntegerField, DecimalField, SubmitField
+from flask_wtf import FlaskForm
+
+class diabetesdataform(FlaskForm):
+    pregnancies = IntegerField("Number of pregnancies", [validators.Required("Please enter your number of pregnancies")])
+    glucose = IntegerField("Your glucose level", [validators.Required("Please enter your glucose level")])
+    bp = IntegerField("Your blood pressure", [validators.Required("Please enter your Blood pressure")])
+    st = IntegerField("Your Skin Thickness", [validators.Required("Enter the skin thickness")])
+    insulin = IntegerField("Insulin", [validators.Required("Enter insulin level")])
+    bmi = DecimalField("BMI", [validators.Required("Enter your BMI")])
+    age = IntegerField("Age", [validators.Required("Enter your age")])
+    
